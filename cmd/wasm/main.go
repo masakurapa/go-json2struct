03eadcf34bc25a6a8f46e3560975aba5c99fc49c
masakurapa <rabasan1031@gmail.com>
2022-02-26 02:09:05 +0900

    add tool layout

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
 
 	"github.com/masakurapa/go-json2struct/pkg/j2s"
@@ -17,7 +16,8 @@ func json2struct(js.Value, []js.Value) interface{} {
 	input := js.Global().Get("document").Call("getElementById", "input").Get("value").String()
 	output, err := j2s.Convert(input)
 	if err != nil {
-		fmt.Println(err)
+		js.Global().Get("document").Call("getElementById", "output").Set("value", err.Error())
+		return nil
 	}
 	js.Global().Get("document").Call("getElementById", "output").Set("value", output)
 	return nil

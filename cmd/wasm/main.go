b9f6747d603ae1f4ed3aa280b608bbc5a20ed87d
masakurapa <rabasan1031@gmail.com>
2022-02-26 21:22:44 +0900

    add copy button

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	ch := make(chan struct{})
 	js.Global().Set("json2struct", js.FuncOf(json2struct))
+	js.Global().Set("copyClipboard", js.FuncOf(copyClipboard))
 	<-ch
 }
 
@@ -20,5 +21,11 @@ func json2struct(js.Value, []js.Value) interface{} {
 		return nil
 	}
 	js.Global().Get("document").Call("getElementById", "output").Set("value", output)
+	return nil
+}
+
+func copyClipboard(js.Value, []js.Value) interface{} {
+	output := js.Global().Get("document").Call("getElementById", "output").Get("value").String()
+	js.Global().Get("navigator").Get("clipboard").Call("writeText", output)
 	return nil
 }
